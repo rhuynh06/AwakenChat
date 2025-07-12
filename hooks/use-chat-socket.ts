ce2a6220dba7a4e3a59f9e601ac11a00558900fc
ryan huynh <ryanhuynh200604@gmail.com>
2025-07-12 13:42:40 -0700

    Implemented auto-scroll and dynamic message loading when exceeding viewport limit

--- hooks/use-chat-socket.ts
+++ hooks/use-chat-socket.ts
@@ -1,10 +1,9 @@
 import { useSocket } from "@/components/providers/socket-provider";
 import { Member, Message, Profile } from "@prisma/client";
 import { useQueryClient } from "@tanstack/react-query";
-import { add } from "date-fns";
 import { useEffect } from "react";
 
-type useChatSocketProps = {
+type ChatSocketProps = {
     addKey: string;
     updateKey: string;
     queryKey: string;
