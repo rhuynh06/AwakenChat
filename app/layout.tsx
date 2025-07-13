d32d91726b9c66f31cc803f4f79e580ab01d8cd1
ryan huynh <ryanhuynh200604@gmail.com>
2025-07-12 20:55:15 -0700

    removed unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,5 @@
 import { type Metadata } from 'next'
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedOut,
-  SignedIn,
-  UserButton
-} from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
@@ -41,19 +34,6 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
               <QueryProvider>
                 {children}
               </QueryProvider>
-              {/* <header className="flex justify-end items-center p-4 gap-4 h-16">
-                <SignedOut>
-                  <SignInButton />
-                  <SignUpButton>
-                    <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                      Sign Up
-                    </button>
-                  </SignUpButton>
-                </SignedOut>
-                <SignedIn>
-                  <UserButton />
-                </SignedIn>
-              </header> */}
             </SocketProvider>
           </ThemeProvider>
         </body>
